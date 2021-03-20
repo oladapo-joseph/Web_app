02bd1b124ed3b6341c1c03a76a8a5f369b935369
Adeleke Oladapo Joseph <oladapo_joseph@yahoo.com>
2021-03-20 20:26:00 +0100

    remote development

---
+++ home.py
@@ -0,0 +1,38 @@
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+import os 
+
+os.chdir('C:/Users/ADELEKE OLADAPO/Documents/Vetsark/Vetsark/sample_projects/web_app/')
+
+app = Flask(__name__, template_folder='templates')
+
+model = load_model('model/test_model')
+cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen, round = 0)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
